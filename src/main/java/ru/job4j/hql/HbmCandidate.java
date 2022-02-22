9409d08ff974ac198be4a544de89c6db21345eeb
AshleySunsine <sgt16@yandex.ru>
2022-02-22 19:27:08 +0300

    0. HQL [#6874 #267002]01

--- src/main/java/ru/job4j/hql/HbmCandidate.java
+++ src/main/java/ru/job4j/hql/HbmCandidate.java
@@ -8,6 +8,9 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.model.Candidateee;
 import ru.job4j.model.VacansionBase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HbmCandidate {
 
     public static void main(String[] args) {
@@ -53,15 +56,17 @@ public class HbmCandidate {
     }
 
     public static Candidateee getCandidate(int id, Session session) {
-          Candidateee cand = (Candidateee) session.createQuery("from Candidateee c where c.id = :cid").setParameter("cid", 1).uniqueResult();
+          Candidateee cand = (Candidateee) session.createQuery("from Candidateee c where c.id = :cid")
+                  .setParameter("cid", 1)
+                  .uniqueResult();
          System.out.println(cand);
          return cand;
     }
 
-    public static Candidateee getCandidate(String name, Session session) {
-        Candidateee cand = (Candidateee) session.createQuery("from Candidateee c where c.name = :cname").setParameter("cname", "Nikita").uniqueResult();
-        System.out.println(cand);
-        return cand;
+    public static List<Candidateee> getCandidate(String name, Session session) {
+        return session.createQuery("from Candidateee c where c.name = :cname")
+                .setParameter("cname", "Nikita")
+                .list();
     }
 
     public static void setCandidate(int id, String newName, Session session) {
